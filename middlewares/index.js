f8c0e8ae4325fab51e26479eacad72e6a9c986db
Rdk001 <rmaslenchenko95@gmail.com>
2023-05-16 23:33:58 +0200

    completed all tasks

---
+++ middlewares/index.js
@@ -0,0 +1,8 @@
+const validateBody = require("./validateBody");
+const validateEmptyBody = require("./validateEmptyBody");
+const isValidId = require("./isValidId");
+module.exports = {
+  validateBody,
+  validateEmptyBody,
+  isValidId,
+};
--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,25 +1,44 @@
-const express = require('express')
+const express = require("express");
 
-const router = express.Router()
+const ctrl = require("../../controllers/contacts");
 
-router.get('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+const {
+  validateBody,
+  isValidId,
+  validateEmptyBody,
+} = require("../../middlewares");
 
-router.get('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+const { addSchema, updateStatusSchema } = require("../../models/contact");
 
-router.post('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+const router = express.Router();
 
-router.delete('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.get("/", ctrl.listContacts);
 
-router.put('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.get("/:id", isValidId, ctrl.getContactById);
 
-module.exports = router
+router.post(
+  "/",
+  validateEmptyBody("Missing fields"),
+  validateBody(addSchema),
+  ctrl.addContact
+);
+
+router.delete("/:id", isValidId, ctrl.removeContact);
+
+router.put(
+  "/:id",
+  validateEmptyBody("Missing fields"),
+  isValidId,
+  validateBody(addSchema),
+  ctrl.updateContact
+);
+
+router.patch(
+  "/:id/favorite",
+  isValidId,
+  validateEmptyBody("Missing field favorite"),
+  validateBody(updateStatusSchema),
+  ctrl.updateStatusContact
+);
+
+module.exports = router;
